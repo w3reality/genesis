847dca723439baf6e8fd826e2ae90c12e6a15c56
jayd <j@w3reality.com>
2019-03-18 14:43:14 +0700

    bang!

---
+++ src/index.js
@@ -0,0 +1,32 @@
+
+const SDK = window.requirejs('w3reality-sdk');
+// console.log('SDK:', SDK);
+
+class App extends SDK.App {
+    // override
+    static createWorld() {
+        const [world, chunkSize] = super.createWorld();
+        world.createFlatWorld(6, BLOCK.CONCRETE);
+        world.setSpawnPose([4.0, 1.5, 8, -Math.PI/16, -Math.PI/2, 0]);
+        return [world, chunkSize];
+    }
+
+    // override
+    constructor(data, name="") {
+        super(data, name);
+        this.set_block(0, 0, 6, BLOCK.MODEL_TEST);
+        this.say('welcome');
+    }
+
+    // override
+    update() {
+        super.update();
+    }
+
+    // override
+    free() {
+        super.free();
+    }
+}
+
+export default App;
